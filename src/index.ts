b6f05f25a7f33c599a6fc9c253e1ea16cad4f7ec
Tamay Aykut <tamay.aykut@outlook.com>
2022-12-09 16:11:43 -0800

    mint nfts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,109 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
+import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js"
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  NftWithToken,
+} from "@metaplex-foundation/js"
+import * as fs from "fs"
+
+const tokenName = "Token Name"
+const description = "Description"
+const symbol = "SYMBOL"
+const sellerFeeBasisPoints = 100
+const imageFile = "new.png"
+
+// create NFT
+async function createNft(
+  metaplex: Metaplex,
+  uri: string
+): Promise<NftWithToken> {
+  const { nft } = await metaplex
+    .nfts()
+    .create({
+      uri: uri,
+      name: tokenName,
+      sellerFeeBasisPoints: sellerFeeBasisPoints,
+      symbol: symbol,
+    })
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+  )
+
+  return nft
+}
+
+async function updateNft(
+  metaplex: Metaplex,
+  uri: string,
+  mintAddress: PublicKey
+) {
+  // get "NftWithToken" type from mint address
+  const nft = await metaplex.nfts().findByMint({ mintAddress })
+
+  // omit any fields to keep unchanged
+  await metaplex
+    .nfts()
+    .update({
+      nftOrSft: nft,
+      name: tokenName,
+      symbol: symbol,
+      uri: uri,
+      sellerFeeBasisPoints: sellerFeeBasisPoints,
+    })
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+  )
+}
 
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
 
   console.log("PublicKey:", user.publicKey.toBase58())
+
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    )
+  // file to buffer
+  const buffer = fs.readFileSync("src/" + imageFile)
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, imageFile)
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: tokenName,
+      description: description,
+      image: imageUri,
+    })
+
+  // console.log("metadata uri:", uri)
+  // await createNft(metaplex, uri)
+  // console.log("nft created")
+
+  // You can get this from the Solana Explorer URL 
+  const mintAddress = new PublicKey("8v5zR8AWPirEHJ2u5fcCNXqXk8YwxDT4KF8mpMYu5RDQ")
+  await updateNft(metaplex, uri, mintAddress)
+  console.log("nft updated")
+
 }
 
 main()
